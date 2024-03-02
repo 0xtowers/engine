d40ee1ce353dd07d56025c2a673b52d152a3c7a8
xh3b4sd <xh3b4sd@gmail.com>
2024-03-02 19:28:32 +0100

    add first unit tests for leading tower (#2)

--- engine.go
+++ engine.go
@@ -6,6 +6,7 @@ type Config struct {
 	// Dwn is the countdown duration that has to pass without any further deposits
 	// in order for the leading tower to win.
 	Dwn time.Duration
+	Now func() time.Time
 }
 
 type Engine struct {
@@ -13,6 +14,8 @@ type Engine struct {
 	dep map[int]int
 	dwn time.Duration
 	gam int
+	lea [3]int
+	now func() time.Time
 	tim time.Time
 	tnx map[string]int
 	tow map[int]int
@@ -21,13 +24,17 @@ type Engine struct {
 
 func New(c Config) *Engine {
 	if c.Dwn == 0 {
-		c.Dwn = time.Hour
+		c.Dwn = 1 * time.Hour
+	}
+	if c.Now == nil {
+		c.Now = func() time.Time { return time.Now().UTC() }
 	}
 
 	return &Engine{
 		bal: map[string]int{},
 		dep: map[int]int{},
 		dwn: c.Dwn,
+		now: c.Now,
 		tnx: map[string]int{},
 		tow: map[int]int{
 			1: 0,
